c2ff65e1a1256d59e26ab4389940b2c5a426d61b
Supritha <smehalingam3@gatech.edu>
2019-02-09 12:40:40 -0500

    update1

--- app/src/main/java/csx060/uga/edu/uga_hack_4_app/LoginActivity.java
+++ app/src/main/java/csx060/uga/edu/uga_hack_4_app/LoginActivity.java
@@ -2,13 +2,35 @@ package csx060.uga.edu.uga_hack_4_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button logInButton;
+    private Button signUpButton;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logInButton = (Button) findViewById(R.id.signInButton);
+        signUpButton = (Button) findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startSignIn();
+            }
+        });
+
+    }
+
+    private void startSignIn(){
+        
     }
 }
 
