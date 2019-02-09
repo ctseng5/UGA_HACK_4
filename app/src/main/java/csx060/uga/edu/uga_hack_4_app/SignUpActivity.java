39f3cae815312a8adb5048be57e72d8502782694
ctseng5 <ctseng1992@gmail.com>
2019-02-09 11:49:46 -0500

    Class update

---
+++ app/src/main/java/csx060/uga/edu/uga_hack_4_app/SignUpActivity.java
@@ -0,0 +1,13 @@
+package csx060.uga.edu.uga_hack_4_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
