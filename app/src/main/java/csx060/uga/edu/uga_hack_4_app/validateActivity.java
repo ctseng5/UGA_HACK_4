4354faa3b496ff1b28ee0cd01460dfa4e1b94494
ctseng5 <ctseng1992@gmail.com>
2019-02-09 11:42:52 -0500

    mehg

--- app/src/main/java/csx060/uga/edu/uga_hack_4_app/validateActivity.java
+++ app/src/main/java/csx060/uga/edu/uga_hack_4_app/validateActivity.java
@@ -1,6 +1,13 @@
 package csx060.uga.edu.uga_hack_4_app;
 
+import android.util.Log;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Pattern;
 
 public class validateActivity extends AsyncTask<Void, Void, String.>{
     private Exception exception;
