b59639ffd242c12a2eae723b3664ac0b15f231fd
Jordan McCombs <40576361+Jordan935@users.noreply.github.com>
2019-02-10 00:51:57 -0500

    update

--- app/src/main/java/csx060/uga/edu/uga_hack_4_app/HomeFragment.java
+++ app/src/main/java/csx060/uga/edu/uga_hack_4_app/HomeFragment.java
@@ -1,5 +1,34 @@
 package csx060.uga.edu.uga_hack_4_app;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +40,15 @@ import android.view.ViewGroup;
 
 
 public class HomeFragment extends Fragment {
+    public final static int QRcodeWidth = 500 ;
+    private static final String IMAGE_DIRECTORY = "/QRcodeDemonuts";
+    Bitmap bitmap ;
+    private EditText etqr;
+    private ImageView iv;
+    private Button btn;
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,5 +56,7 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
 
 }
